e362bb2a2d035fb5b8f78ba2a8804fc7306080be
harshglobal <harsh.gawande@gmail.com>
2022-11-17 13:34:14 +0530

    Settings

--- level_two_project/settings.py
+++ level_two_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +21,8 @@ TEMPLATES_DIR = BASE_DIR / "templates"
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jzubx)v)#!@lx6brv3i8mah@b6+8^b0jmsqww2ymm=$jn*==+e'
-
+# SECRET_KEY = 'django-insecure-jzubx)v)#!@lx6brv3i8mah@b6+8^b0jmsqww2ymm=$jn*==+e'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -118,14 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS=[
-    BASE_DIR / 'static',
-]
+STATIC_URL = 'static/'
+STATICFILES_DIR = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
